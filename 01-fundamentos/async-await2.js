e808aea27f86fe3102ad3d8c44a0b356e95712a8
Yuly Velasquez <marcela.velasquez@netuxtecnologia.com>
2020-02-06 10:27:36 -0500

    Add comments to the code

--- 01-fundamentos/async-await2.js
+++ 01-fundamentos/async-await2.js
@@ -51,8 +51,7 @@ getInformation(3)
 .catch(err => console.log(err))
 
 
-
-
+//Otra forma
 // getEmployee(3)
 // .then(employee => {
 //   getSalary(employee)
@@ -63,7 +62,7 @@ getInformation(3)
 // })
 // .catch(err => console.log(err))
 
-
+// Detecta promesas sin resolver
 // process.on('unhandledRejection', (reason, p) => {
 //   console.log('Unhandled Rejection at:', p, 'reason:', reason);
 //   // application specific logging, throwing an error, or other logic here
